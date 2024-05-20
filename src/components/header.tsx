836791acf4656886fd1bc5b556d3b28684660e44
Dmarzzucco <ksksks>
2024-05-20 14:47:02 -0300

    update to ts

---
+++ src/components/header.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+const Header: React.FC = () => {
+    return (
+        <header className="header">
+            <a href="#" className="logo">
+                <i className="fa-solid fa-car"></i> <span>AutoMotris</span>
+            </a>
+            <nav className="menu">
+                <ul className="menu-list">
+                    <li className="menu-list_item" style={{ display: 'inline-block' }}>
+                        <a href="#">Home</a>
+                    </li>
+                    <li className="menu-list_item" style={{ display: 'inline-block' }}>
+                        <a href="#">Features</a>
+                    </li>
+                    <li className="menu-list_item" style={{ display: 'inline-block' }}>
+                        <a href="#">Price</a>
+                    </li>
+                    <li className="menu-list_item" style={{ display: 'inline-block' }}>
+                        <a href="#">Contact</a>
+                    </li>
+                    <li className="menu-list_item" style={{ display: 'inline-block' }}>
+                        <a href="#" className="book-apointment">Book Apointment</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;

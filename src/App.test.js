078f965d3a5193cb5872c47d1ec6cca0391344e0
Dmarzzucco <ksksks>
2024-05-20 14:46:19 -0300

    delete

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-// App.test.js
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders the component without errors', () => {
-  render(<App />);
-});
